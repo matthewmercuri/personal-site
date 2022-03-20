9fe57af302ab70e2ff13a7359333442a83c0f6fb
Matthew Mercuri <mercurimatthew@gmail.com>
2022-03-20 15:06:56 -0400

    mobile nav slide in complete

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import ThemeIndicator from './components/ThemeIndicator'
 import styles from './NavBar.module.scss'
 
@@ -7,18 +8,32 @@ type INProps = {
 }
 
 const NavBar = ({toggleTheme, isDarkTheme}: INProps) => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
   return (
-    <nav className={styles.navBar}>
+    <nav className={isNavOpen ? `${styles.navBar} ${styles.navBarOpen}` : styles.navBar}>
       <div className={styles.navBarContent}>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>resources</li>
+        </ul>
         <ThemeIndicator toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
-        <div className={styles.navBarContentHamburger}>
+        <div className={styles.navBarContentHamburger} onClick={() => setIsNavOpen(!isNavOpen)}>
           <div />
           <div />
           <div />
         </div>
       </div>
+      <div className={isNavOpen ? `${styles.navBarMobileContent} ${styles.navBarMobileContentOpen}` : styles.navBarMobileContent}>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>resources</li>
+        </ul>
+      </div>
     </nav>
   )
 }
-  
+
 export default NavBar
