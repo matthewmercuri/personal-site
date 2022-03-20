2f1b66a7423dd4cf8d8bfccd26a24bdf10442446
Matthew Mercuri <mercurimatthew@gmail.com>
2022-03-20 18:54:27 -0400

    commented out type logo

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -13,7 +13,7 @@ const NavBar = ({toggleTheme, isDarkTheme}: INProps) => {
   return (
     <nav className={isNavOpen ? `${styles.navBar} ${styles.navBarOpen}` : styles.navBar}>
       <div className={styles.navBarContent}>
-        <span>matthewmercuri</span>
+        {/* <span>matthewmercuri</span> */}
         <ul>
           <li>home</li>
           <li>about</li>
