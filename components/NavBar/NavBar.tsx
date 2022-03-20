2942da089f819db1668828cd00876a9ce560adf4
Matthew Mercuri <mercurimatthew@gmail.com>
2022-03-19 22:39:20 -0400

    hamburger menu added to nav

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,6 +3,13 @@ import styles from './NavBar.module.scss'
 const NavBar = () => {
   return (
     <nav className={styles.navBar}>
+      <div className={styles.navBarContent}>
+        <div className={styles.navBarContentHamburger}>
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
     </nav>
   )
 }
