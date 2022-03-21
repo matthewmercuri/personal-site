0169880856bd92d06889560e87adb885eb36f3c3
Matthew Mercuri <mercurimatthew@gmail.com>
2022-03-20 20:34:10 -0400

    hero animation working

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -5,8 +5,13 @@ type INProps = {
 
 const Hero = ({}: INProps) => {
   return (
-    <></>
+    <div className={`content-container ${styles.hero}`}>
+      <div className={styles.heroAnimation}>
+        <img src='/assets/atomcoin.svg' alt='atom coin icon' />
+        <div className={styles.heroAnimationShadow} />
+      </div>
+    </div>
   )
 }
-  
+
 export default Hero
