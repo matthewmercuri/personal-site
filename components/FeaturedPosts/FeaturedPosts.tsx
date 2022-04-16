e98a0dd9f38740384c2089584bf9f35bc992fbc8
Matthew Mercuri <mercurimatthew@gmail.com>
2022-04-16 12:31:39 -0400

    changing arrow icon based on theme

--- components/FeaturedPosts/FeaturedPosts.tsx
+++ components/FeaturedPosts/FeaturedPosts.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import { useContext } from 'react'
+import { ThemeContext } from '../../services/theme.context'
 import { grayMatterParsedPostsType } from '../../types/post.types'
 import PostClickable from '../PostClickable'
 import styles from './FeaturedPosts.module.scss'
@@ -8,6 +10,7 @@ type INProps = {
 }
 
 const FeaturedPosts = ({ posts }: INProps) => {
+  const { isDarkTheme } = useContext(ThemeContext)
   return (
     <section className={`content-container ${styles.featuredPosts}`}>
       <h2 className='section-heading'>FEATURED</h2>
@@ -17,7 +20,11 @@ const FeaturedPosts = ({ posts }: INProps) => {
       <Link href={'/blog'}>
         <div className={styles.featuredPostsLink}>
           <span>all posts</span>
-          <img src={'/icons/arrow_right_dark.svg'} alt='all blog post link icon, click here to view all posts' />
+          {isDarkTheme ? (
+            <img src={'/icons/arrow_right_light.svg'} alt='all blog post link icon, click here to view all posts' />
+          ) : (
+            <img src={'/icons/arrow_right_dark.svg'} alt='all blog post link icon, click here to view all posts' />
+          )}
           <div />
         </div>
       </Link>
