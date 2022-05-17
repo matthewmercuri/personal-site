2b02f31d6833c07db4182438256bdf6c037dc3ad
Matthew Mercuri <mercurimatthew@gmail.com>
2022-05-17 17:19:35 -0400

    added page title to blog index

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from 'next'
 import getPostsFromLocalFolder from '../../services/getPostsFromLocalFolder'
 import { grayMatterParsedPostsType } from '../../types/post.types'
 import PostClickable from '../../components/PostClickable'
+import Head from 'next/head'
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const posts = getPostsFromLocalFolder()
@@ -19,10 +20,15 @@ type INProps = {
 
 const Blog = ({posts}: INProps) => {
   return (
-   <section className='content-container'>
-     <h1 className='section-heading no-top-margin'>BLOG POSTS</h1>
-     {posts.map((post) => <PostClickable key={post.data.slug} postData={post} />)}
-   </section>
+    <>
+    <Head>
+      <title>matthewmercuri | blog</title>
+    </Head>
+    <section className='content-container'>
+      <h1 className='section-heading no-top-margin'>BLOG POSTS</h1>
+      {posts.map((post) => <PostClickable key={post.data.slug} postData={post} />)}
+    </section>
+   </>
   )
 }
 
